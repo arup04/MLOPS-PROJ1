8584238e8902fd5f94dd53db9e75d393ca18da01
arup04 <arup46606@gmail.com>
2026-01-24 22:45:21 +0530

    data ingestion added

--- demo.py
+++ demo.py
@@ -1,21 +1,27 @@
-# below code is to check the logging config
+# # below code is to check the logging config
+# # from src.logger import logging
+
+# # logging.debug("This is a debug message.")
+# # logging.info("This is an info message.")
+# # logging.warning("This is a warning message.")
+# # logging.error("This is an error message.")
+# # logging.critical("This is a critical message.")
+
+# # --------------------------------------------------------------------------------
+
+# # below code is to check the exception config
 # from src.logger import logging
+# from src.exception import MyException
+# import sys
 
-# logging.debug("This is a debug message.")
-# logging.info("This is an info message.")
-# logging.warning("This is a warning message.")
-# logging.error("This is an error message.")
-# logging.critical("This is a critical message.")
+# try:
+#     a = 1+'Z'
+# except Exception as e:
+#     logging.info(e)
+#     raise MyException(e, sys) from e
 
-# --------------------------------------------------------------------------------
 
-# below code is to check the exception config
-from src.logger import logging
-from src.exception import MyException
-import sys
+from src.pipline.training_pipeline import TrainPipeline
 
-try:
-    a = 1+'Z'
-except Exception as e:
-    logging.info(e)
-    raise MyException(e, sys) from e
+pipline = TrainPipeline()
+pipline.run_pipeline()
